7ab12f3082e41189606898fa889de2857bd7623f
Vishnuala09 <vishnu.ala0009@gmail.com>
2023-08-17 23:57:06 +0530

    Create shortdemo.java

---
+++ shortdemo.java
@@ -0,0 +1,16 @@
+class shortdemo{
+	public static void main(String[] args){
+		short s = 32767;
+		short t = -32768;
+		short u = 32769;
+		short a = -32769;
+		short h = 78.99;
+		short w= false;
+		System.out.println("The value of s:" + s);
+		System.out.println("The value of s:" + t);
+		System.out.println("The value of s:" + u);
+		System.out.println("The value of s:" + a);
+		System.out.println("The value of s:" + h);
+		System.out.println("The value of s:" + w);
+}
+}
